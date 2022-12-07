f7ca0ce81a4cc4e74868073087f1471051cf45bc
Pi Pi <diwas118151@gmail.com>
2022-12-07 14:56:42 +0700

    jwt authentication and improve login and register

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -1,18 +1,29 @@
 const UserModel = require('../models/UserModel')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 class AuthController {
    // [POST]: /auth/login
    loginUser = async function (req, res) {
       console.log('loginUser')
-      const { username, password } = req.body
+      const { username } = req.body
 
       try {
          const user = await UserModel.findOne({ username })
 
          if (user) {
-            const validity = await bcrypt.compare(password, user.password)
-            validity ? res.status(200).json(user) : res.status(400).json('Wrong password')
+            const validity = await bcrypt.compare(req.body.password, user.password)
+            if (!validity) {
+               res.status(400).json('Wrong password')
+            }
+            const { password, ...otherDetails } = user._doc
+            const token = jwt.sign(
+               { username: user.username, id: user._id },
+               process.env.JWT_KEY,
+               { expiresIn: '1h' }
+            )
+
+            res.status(200).json({ user: otherDetails, token })
          } else {
             res.status(403).json('User does not exists')
          }
@@ -24,16 +35,26 @@ class AuthController {
    // [POST]: /auth/register
    registerUser = async function (req, res) {
       console.log('registerUser')
-      const { username, password, firstName, lastName } = req.body
-
       // hash password
       const salt = await bcrypt.genSalt(10)
-      const hashedPass = await bcrypt.hash(password, salt)
+      const hashedPass = await bcrypt.hash(req.body.password, salt)
+      req.body.password = hashedPass
+      const { username } = req.body
 
       try {
-         const newUser = new UserModel({ username, password: hashedPass, firstName, lastName })
-         await newUser.save()
-         res.status(200).json(newUser)
+         const oldUser = await UserModel.findOne({ username })
+         if (oldUser) {
+            res.status(400).json('Username is already registered')
+         }
+         const newUser = new UserModel(req.body)
+         const user = await newUser.save()
+         const { password, ...otherDetails } = newUser._doc
+
+         const token = jwt.sign({ username: user.username, id: user._id }, process.env.JWT_KEY, {
+            expiresIn: '1h',
+         })
+
+         res.status(200).json({ user: otherDetails, token })
       } catch (err) {
          res.status(500).json({ message: err.message })
       }
