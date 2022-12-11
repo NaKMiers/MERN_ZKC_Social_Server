834ac9e6b9861d4f7e605090621223079dd440e5
Pi Pi <diwas118151@gmail.com>
2022-12-11 17:27:09 +0700

    chat complete

--- src/app/controllers/MessageController.js
+++ src/app/controllers/MessageController.js
@@ -2,28 +2,28 @@ const MessageModel = require('../models/MessageModel')
 
 class MessageController {
    // [POST]: /messages/
-   addMessage = async function (req, res, next) {
+   addMessage = async function (req, res) {
       console.log('addMessage')
 
-      const {chatId, senderId, text} = req.body
+      const { chatId, senderId, text } = req.body
       try {
-         const message = new MessageModel({chatId, senderId, text})
+         const message = new MessageModel({ chatId, senderId, text })
          const result = await message.save()
          res.status(200).json(result)
       } catch (err) {
-         res.status(500).json({message: err.message})
+         res.status(500).json({ message: err.message })
       }
    }
 
    // [GET]: /messages/:chatId
-   getMessages = async function (req, res, next) {
+   getMessages = async function (req, res) {
       console.log('getMessages')
 
       try {
-         const result = await MessageModel.find({chatId: req.params.chatId})
+         const result = await MessageModel.find({ chatId: req.params.chatId })
          res.status(200).json(result)
       } catch (err) {
-         res.status(500).json({message: err.message})
+         res.status(500).json({ message: err.message })
       }
    }
 }
